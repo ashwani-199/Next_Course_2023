c20e46aa5b9a03a352a9cb3fcf6c7781e89f710b
Ashwani kumar <65064582+ashwani-199@users.noreply.github.com>
2023-11-30 15:00:10 +0530

    Second

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,23 @@
 import Hero from "@/components/Hero";
-import Image from "next/image";
+import About from "@/components/About";
+import Services from "@/components/Service";
+import Work from "@/components/Work";
+import Reviews from "@/components/Reviews";
+import Cta from "@/components/Cta";
+
 
 
 const Home = () => {
   return (
     <main>
       <Hero/>
+      <About />
+      <Services/>
+      <Work/>
+      <Reviews />
+      <Cta/>
     </main>
   );
 }
 
-export default Hero;
+export default Home;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,17 @@
+import Socials from "./Socials";
+
+
 const Footer = () => {
-    return <footer>Footer</footer> 
+    return <footer className="bg-secondary py-12">
+        <div className="container mx-auto">
+            <div className="flex flex-col items-center justify-between">
+                <Socials containerStyles='flex gap-x-6' iconStyles='text-primary dark:text-white/70 text=-[20px] hover:text-white dark:hover:text-primary'/>
+                <div className="text-muted-foreground">
+                    Copyright &copy; Ashwani Kumar. All rights reserved.
+                </div>
+            </div>
+        </div>
+    </footer> 
 };
 
 export default Footer;
